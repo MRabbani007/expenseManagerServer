1430463b20ca28ab7a1b0646a7ef320db3572a98
Mohamad Rabbani <mrabbani@outlook.com>
2024-02-20 11:04:43 +0500

    cors fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 // Cross Origin Resource Sharing
 const allowedOrigins = [
-  "https://mrabbani007.github.io/expenseManager/",
+  "https://mrabbani007.github.io/",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
   "http://localhost:5173",
