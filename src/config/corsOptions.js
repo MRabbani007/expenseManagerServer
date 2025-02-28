b59ac4a41feff5db9501d59a2c8e8520a3272d56
Mohamad Rabbani <143613695+MRabbani007@users.noreply.github.com>
2025-02-28 23:34:39 +0500

    bug fix
    cors options

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,17 +1,19 @@
 import allowedOrigins from "./allowedOrigins.js";
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  // origin: (origin, callback) => {
+  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"));
+  //   }
+  // },
   optionsSuccessStatus: 200,
   // origin: true,
   methods: ["GET", "HEAD", "PATCH", "POST", "DELETE", "OPTIONS"],
-  credentials: true,
+  origin: ["https://mrabbani007.github.io", "http://localhost:5173"], // Allow only frontend
+  credentials: true, // Allow cookies/sessions
+  allowedHeaders: ["Content-Type", "Authorization"],
   // allowedHeaders: [
   //   "Origin",
   //   "Content-Type",
